260c0287cf3d9aafaab6ed16de61eeb73348e87b
evoulavou <evdoxia.vouvouli@gmail.com>
2026-08-24 14:00:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 from placement import placements
 from notification3 import notifications3
-from sort import organize_files_by_school
+from sort import organize_files_by_school, prepare_pdfs_for_signing
 
 root = tk.Tk()
 root.withdraw()
